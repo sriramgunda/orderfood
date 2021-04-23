57bf6d144ad8f3b6d175f9c9637822fa1704475b
sriram <sriramgunda@yahoo.com>
2021-04-24 00:20:01 +0530

    web page layout desigining

--- static/js/script.js
+++ static/js/script.js
@@ -1,3 +1,14 @@
-function minusitem(){
-	alert(this);
+function incrementValue()
+{
+    var value = parseInt(document.getElementById('one').value, 10);
+    value = isNaN(value) ? 0 : value;
+    value++;
+    document.getElementById('one').value = value;
+}
+function decrementValue()
+{
+    var value = parseInt(document.getElementById('one').value, 10);
+	
+    value = isNaN(value) ? 0 : value--;
+    document.getElementById('one').value = value;
 }
