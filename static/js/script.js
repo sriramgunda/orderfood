f1dbb394256187308ac9b567a632c567a442216b
sriram <sriramgunda@yahoo.com>
2021-04-25 20:40:39 +0530

    web page layout desigining

--- static/js/script.js
+++ static/js/script.js
@@ -25,4 +25,11 @@ function decrementValue(){
 //change cart label
 function changeCartText(txt="Cart Value"){
 	document.getElementById('cart-text').innerHTML = txt;
+	if (txt === "Cart Value"){
+		document.getElementById("empty-cart-id").style.display = "none";
+	}
+	else{
+		document.getElementById("empty-cart-id").style.display = "block";
+	}
+	
 }
