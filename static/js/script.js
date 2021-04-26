6f8bb117f987a33ee06431a8722963ca6f7c59ee
sriram <sriramgunda@yahoo.com>
2021-04-26 22:53:58 +0530

    web page layout desigining

--- static/js/script.js
+++ static/js/script.js
@@ -4,7 +4,7 @@ function incrementValue(){
     value = isNaN(value) ? 0 : value;
     value++;
     document.getElementById('one').value = value;
-	changeCartText();
+	changeCartText(txt="Cart Value", itemscount = value);
 }
 
 // Decrement selected item quantity
@@ -14,20 +14,21 @@ function decrementValue(){
 	value--;
 	if (value <= 0){
 		value = 0;
-		changeCartText(txt="Cart Empty");
+		changeCartText(txt="Cart Empty", itemscount = 0);
 	}
 	else{
-		changeCartText();
+		changeCartText(txt="Cart Value", itemscount = value);
 	}
     document.getElementById('one').value = value;	
 }
 
 //change cart label
-function changeCartText(txt="Cart Value"){
+function changeCartText(txt="Cart Value", itemscount = 0){
 	document.getElementById('cart-text').innerHTML = txt;
 	if (txt === "Cart Value"){
 		document.getElementById("empty-cart-id").style.display = "none";
 		document.getElementById("menu-cart-values-id").style.display = "block";
+		document.getElementById("menu-cart-items-count-id").innerHTML = itemscount + " Items";
 	}
 	else{
 		document.getElementById("empty-cart-id").style.display = "block";
